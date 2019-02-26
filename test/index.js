1ac293c752c9e4a2ec843520f7bd594d3f8a4aa4
Edward Stone <ed@autotelic.com>
2019-02-26 15:10:58 -0800

    add optimist block to async actions if optimistId in meta

--- test/index.js
+++ test/index.js
@@ -2,7 +2,84 @@ import { expect } from 'chai';
 import sinon from 'sinon'
 import jwt from 'jsonwebtoken';
 
-import { TokenApiService, createResponseHandlerWithMeta } from '../src';
+import {
+  TokenApiService,
+  createResponseHandlerWithMeta,
+  createAsyncAction,
+} from '../src';
+
+describe('createAsyncAction', () => {
+  const type = 'OPTIMISTIC_ACTION';
+  const optimistId = '123456';
+  const meta = { optimistId };
+  const payload = { foo: 'bar' };
+
+  const tests = [
+    {
+      desc: 'should add BEGIN for async START',
+      input: 'START',
+      expected: {
+        type: 'OPTIMISTIC_ACTION_START',
+        payload,
+        meta: {
+          ...meta,
+          asyncStep: 'START',
+        },
+        optimist: {
+          type: 'BEGIN',
+          id: optimistId,
+        },
+      },
+    },
+    {
+      desc: 'should add COMMIT for async COMPLETED',
+      input: 'COMPLETED',
+      expected: {
+        type: 'OPTIMISTIC_ACTION_COMPLETED',
+        payload,
+        meta: {
+          ...meta,
+          asyncStep: 'COMPLETED',
+        },
+        optimist: {
+          type: 'COMMIT',
+          id: optimistId,
+        },
+      },
+    },
+    {
+      desc: 'should add REVERT for async FAILED',
+      input: 'FAILED',
+      expected: {
+        type: 'OPTIMISTIC_ACTION_FAILED',
+        payload,
+        meta: {
+          ...meta,
+          asyncStep: 'FAILED',
+        },
+        optimist: {
+          type: 'REVERT',
+          id: optimistId,
+        },
+      },
+    },
+  ];
+
+  tests.forEach((t) => {
+    it(t.desc, () => {
+      const {
+        input,
+        expected,
+      } = t;
+
+      // Act
+      const actual = createAsyncAction(type, input, payload, meta);
+
+      // Assert
+      expect(actual).deep.equal(expected);
+    });
+  });
+});
 
 describe('createResponseHandlerWithMeta', () => {
 
